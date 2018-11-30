703cc88da5bf0d58c54b6ed8ccadbb6b29b49392
afeiszli <alex.feiszli@gmail.com>
2018-11-30 12:18:12 -0500

    openshift-task templates

--- appdev/cicd-setup.sh
+++ appdev/cicd-setup.sh
@@ -14,15 +14,14 @@ oc process -f tasks-pipeline.yaml -n cicd-dev | oc create -f -
 
 
 #oc new-app https://github.com/wkulhanek/openshift-tasks.git --name=openshift-task -n cicd
-oc new-project tasks
-oc new-app eap70-basic-s2i --param SOURCE_REPOSITORY_URL=https://github.com/wkulhanek/openshift-tasks.git --param APPLICATION_NAME=openshift-tasks -n tasks
+#oc new-app eap70-basic-s2i --param SOURCE_REPOSITORY_URL=https://github.com/wkulhanek/openshift-tasks.git --param APPLICATION_NAME=openshift-tasks -n tasks
 
 
-oc policy add-role-to-user edit system:serviceaccount:cicd:jenkins -n tasks
-oc policy add-role-to-group system:image-puller system:serviceaccounts:cicd -n tasks
-oc policy add-role-to-user edit system:serviceaccount:cicd:jenkins -n qa
-oc policy add-role-to-group system:image-puller system:serviceaccounts:cicd -n qa
-oc policy add-role-to-group system:image-puller system:serviceaccounts:qa -n tasks
+oc policy add-role-to-user edit system:serviceaccount:cicd:jenkins -n tasks-build
+oc policy add-role-to-group system:image-puller system:serviceaccounts:cicd -n tasks-build
+oc policy add-role-to-user edit system:serviceaccount:cicd:jenkins -n tasks-qa
+oc policy add-role-to-group system:image-puller system:serviceaccounts:cicd -n tasks-qa
+oc policy add-role-to-group system:image-puller system:serviceaccounts:qa -n tasks-build
 oc create deploymentconfig tasks --image=docker-registry.default.svc.cluster.local:5000/tasks/tasks:promoteToQA -n qa
 oc expose dc tasks --port=8080 -n qa
 oc expose svc tasks  -n qa
