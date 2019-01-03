8dde5b006b2184c8e7e85d82087a7eb20d7286e5
root <root@bastion.ca65.example.opentlc.com>
2019-01-03 16:32:32 +0000

    htpass fix

--- scripts/create_users.sh
+++ scripts/create_users.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 #Create Users
-htpasswd -c -b /etc/origin/maser/htpasswd amy r3dh4t1!
-htpasswd -c -b /etc/origin/maser/htpasswd andrew r3dh4t1!
-htpasswd -c -b /etc/origin/maser/htpasswd brian r3dh4t1!
-htpasswd -c -b /etc/origin/maser/htpasswd betty r3dh4t1!
+#htpasswd -b /etc/origin/master/htpasswd amy r3dh4t1!
+#htpasswd -b /etc/origin/master/htpasswd andrew r3dh4t1!
+#htpasswd -b /etc/origin/master/htpasswd brian r3dh4t1!
+#htpasswd -b /etc/origin/master/htpasswd betty r3dh4t1!
 
 #label users for corp
 oc label user/amy level=AlphaCorp
