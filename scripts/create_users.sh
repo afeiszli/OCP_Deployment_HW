2eaa6cd36ecb176601f3b2a7b227f23e100c4824
root <root@bastion.2633.example.opentlc.com>
2019-01-03 19:32:13 +0000

    users file had commented out lines

--- scripts/create_users.sh
+++ scripts/create_users.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 #Create Users
-#htpasswd -b /etc/origin/master/htpasswd amy r3dh4t1!
-#htpasswd -b /etc/origin/master/htpasswd andrew r3dh4t1!
-#htpasswd -b /etc/origin/master/htpasswd brian r3dh4t1!
-#htpasswd -b /etc/origin/master/htpasswd betty r3dh4t1!
+htpasswd -b /etc/origin/master/htpasswd amy r3dh4t1!
+htpasswd -b /etc/origin/master/htpasswd andrew r3dh4t1!
+htpasswd -b /etc/origin/master/htpasswd brian r3dh4t1!
+htpasswd -b /etc/origin/master/htpasswd betty r3dh4t1!
 
 #label users for corp
 oc label user/amy level=AlphaCorp
