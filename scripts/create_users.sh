c38c0468fa5d709847b2040fbefb4d77417ce53a
root <root@bastion.b0da.example.opentlc.com>
2019-01-04 16:51:57 +0000

    minor script changes

--- scripts/create_users.sh
+++ scripts/create_users.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 #Create Users
-#htpasswd -b /etc/origin/master/htpasswd amy r3dh4t1!
-#htpasswd -b /etc/origin/master/htpasswd andrew r3dh4t1!
-#htpasswd -b /etc/origin/master/htpasswd brian r3dh4t1!
-#htpasswd -b /etc/origin/master/htpasswd betty r3dh4t1!
+htpasswd -b /etc/origin/master/htpasswd amy r3dh4t1!
+htpasswd -b /etc/origin/master/htpasswd andrew r3dh4t1!
+htpasswd -b /etc/origin/master/htpasswd brian r3dh4t1!
+htpasswd -b /etc/origin/master/htpasswd betty r3dh4t1!
 
 oc login -u amy -p r3dh4t1!
 oc login -u andrew -p r3dh4t1!
@@ -18,5 +18,5 @@ oc label user/andrew level=AlphaCorp
 oc label user/brian level=BetaCorp
 oc label user/betty level=BetaCorp
 
-oc adm groups new alpha amy andrew 
-oc adm groups new alpha brian betty
+oc adm groups new alpha amy andrew | true
+oc adm groups new alpha brian betty | true
